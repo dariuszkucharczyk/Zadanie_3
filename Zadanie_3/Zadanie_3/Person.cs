bc9a96d9b06b605ada7a7e61749bb5c0b9f9b3a6
dariuszkucharczyk <dariuszkucharczyk@o2.pl>
2018-09-27 07:33:01 +0200

    first commit

---
+++ Zadanie_3/Zadanie_3/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3
+{
+    class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person (string n1, int a1)
+        {
+            Name = n1;
+            Age = a1;
+        }
+
+        public override string ToString()
+        {
+            return "{" + Name + "}" + "-" + "{" + Age.ToString() + "}";
+        }
+
+        public int CompareTo(Person obj)
+        {
+            return Age.CompareTo(obj.Age);
+        }
+
+    }
+}
---
+++ Zadanie_3/Zadanie_3/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3
+{
+    class Program
+    {
+        static bool b;
+
+        static void PrintAscending (List<Person> collection)
+        {
+            foreach(Person p in collection)
+            {
+                if(p.Age>=18)
+                Console.WriteLine(p.ToString());
+            }
+
+        }
+
+        static void PrintOccurenceOfGivenAge (List<Person> collection, int age)
+        {
+            foreach (Person p in collection)
+            {
+                if (p.Age == age)
+                {
+                    b = true;
+                    break;
+                }
+                else
+                    b = false;
+            }
+
+            Console.WriteLine(b);
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            List<Person> collection = new List<Person>();
+
+            collection.Add(new Person("John", 16));
+            collection.Add(new Person("Gabi", 46));
+            collection.Add(new Person("Michael", 22));
+            collection.Add(new Person("Derek", 9));
+            collection.Add(new Person("Julia", 37));
+            collection.Add(new Person("Robert", 100));
+            collection.Add(new Person("Ana", 10));
+            collection.Add(new Person("Marcin", 53));
+            collection.Add(new Person("Jimmy", 72));
+            collection.Add(new Person("Maria", 18));
+            collection.Add(new Person("Karol", 17));
+
+            collection.Sort();
+
+            PrintAscending(collection);
+
+            PrintOccurenceOfGivenAge(collection, 18);
+
+            PrintOccurenceOfGivenAge(collection, 108);
+
+            Console.ReadKey();
+        }
+
+
+        
+    }
+}
